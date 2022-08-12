a661f5e0f70c06cc85b2780cf29b178a79950fde
Sergio <sherrera725@gmail.com>
2022-08-12 00:02:49 -0700

    Initial Commit

---
+++ TickTacToes/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TickTacToes
+//
+//  Created by Sergio Herrera on 8/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TickTacToes/TickTacToesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTacToesApp.swift
+//  TickTacToes
+//
+//  Created by Sergio Herrera on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TickTacToesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
